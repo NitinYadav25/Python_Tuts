cf5641113684273b700a828d951a29c34544cece
Mr.Nitin Yadav <91445386+NitinYadav25@users.noreply.github.com>
2021-09-27 08:42:22 +0530

    Python Course Codes

---
+++ tut6.py
@@ -0,0 +1,13 @@
+# Please dont remove this line
+"""
+This is a
+Multiline Comment
+"""
+"""
+This is a comment
+"""
+# print("Subscribe NyGaming Point now","Bhai video bhi like kar dena")
+# print("next line")
+# print("C:\'narry")
+print("Nitin is \n good boy \t1") #comment after statement
+
