3e9231a1b63392b32cc7b8699df59ea555202670
Mr.Nitin Yadav <91445386+NitinYadav25@users.noreply.github.com>
2021-10-13 15:03:26 +0530

    Add files via upload

---
+++ JoinFunc.py
@@ -0,0 +1,7 @@
+lis = ["Rohan", "Akshat", "Nitin", "Kartik", "Rudraksh", "Priya", "Jackson"]
+
+# for item in lis:
+#     print(item, "and", end=" ")
+
+a = ", ".join(lis)
+print(a, "Other my friends name")
---
+++ tutmain1.py
@@ -0,0 +1,15 @@
+
+
+def printnit(string):
+    return f"Ye string nitin ko de de rohan {string}"
+
+def add(num1, num2):
+    return num1 + num2 + 5
+
+
+print("and the name is", __name__)
+
+if __name__ == '__main__':
+    print(printnit("Nitin1"))
+    o = add(4, 6)
+    print(o)
