20daef09292da13184551c6b6615b0746a89c229
Mr.Nitin Yadav <91445386+NitinYadav25@users.noreply.github.com>
2021-11-03 09:46:21 +0530

    Add files via upload

---
+++ oops6.py
@@ -0,0 +1,28 @@
+class Employee:
+    no_of_leaves = 8
+
+    def __init__(self, aname, asalary, arole):
+        self.name = aname
+        self.salary = asalary
+        self.role = arole
+
+    def printdetails(self):
+        return f" The Name is {self.name}. Salary is {self.salary} and role is {self.role}"
+
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+    @classmethod
+    def from_dash(cls, string):
+        return cls(*string.split("-"))
+
+    @staticmethod
+    def printgood(string):
+        print("This is good " + string)
+
+nitin = Employee("Nitin", 255, "Instructor")
+aayush = Employee("aayush", 455, "student")
+karan = Employee.from_dash("Karan-480-student")
+
+Employee.printgood("karan")
